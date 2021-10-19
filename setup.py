6472f657127189c3a77e29ec2d511ac1268271f4
Qishuai Zhong <qishuai.zhong@shopee.com>
2021-10-19 17:42:05 +0800

    modify the version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="mp-socket-logging",
-    version="0.1.1",
+    version="0.1.0",
     description="A small toolkit for python multiprocessing file logging",
     long_description=readme,
     author="Zhong Qishuai",
